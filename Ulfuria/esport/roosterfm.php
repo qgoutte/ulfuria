aa48652acc93b708ab71b3f2ec232443d74f2f06
qgoutte <quentingoutte@gmail.com>
2018-09-20 22:06:08 +0200

    delete useless pages and update roosterfm.php

--- Ulfuria/esport/roosterfm.php
+++ Ulfuria/esport/roosterfm.php
@@ -24,21 +24,32 @@ include 'header.html';
 				<header class="page-header">
 					<h1 class="page-title">L'équipe Football Manager</h1>
 				</header>
-				<p>Notre equipe est composée de 3 membres que voici :
+				<p>Notre equipe est composée de 2 membres que voici :
 				</p>
 				
-				<h3>Erforcewan</h3>
+				<!--<h3>Erforcewan</h3>
 				<h6>Joueur Football Manager</h6>
 				<p>Erforcewan mais aussi appellé Paquito est Fmiste depuis FM15 il participe actuellement à #QSLMMO18 et compte faire d'autres tournois sur le jeu.</p>
 				<p>Ses réseaux sociaux :
 					<ul>
 						<li><a href="https://twitter.com/errwanlg" onclick="window.open(this.href); return false;"><i class="fab fa-twitter"></i> <b>@errwanlg</b></a></li>
 					</ul>
-				</p>
+				</p> -->
 				
 				<h3>Kyristepiu</h3>
 				<h6>Joueur & Référent Football Manager</h6>
-				<p>Joueur de Football Manager depuis FM12, Kyristepiu commence les tournois FM après de nombreuses heures et parties sur le jeu. Actuellement participant des #QSLMMO18 de Manager-Online</p>
+                                <p>Joueur de Football Manager depuis FM12, Kyristepiu commence les tournois FM après de nombreuses heures et parties sur le jeu. <br> 
+                                Tournois réalisés : 
+                                    <ul>
+                                        <li><a href="http://manageronline.fr/fm18-qui-sera-le-meilleur-manager-online/" onclick="window.open(this.href); return false;">QSLMMO18 - Huitième de finale</a></li>
+                                        <li><a href="https://www.toornament.com/tournaments/1520300247544266752/information" onclick="window.open(this.href); return false;">ELSC - 19ème de la poule Bleue</a></li>
+                                    </ul>
+                                Stories en cours/réalisées : 
+                                    <ul>
+                                        <li><a href="https://fmslife.fr/t/road-trip-in-america/3540" onclick="window.open(this.href); return false;">FM18 - Road trip in America ft. Silverip</a></li>
+                                       
+                                    </ul>
+                                </p>
 				<p>Ses réseaux sociaux :
 					<ul>
 						<li><a href="https://twitter.com/Kyristepiu" onclick="window.open(this.href); return false;"><i class="fab fa-twitter"></i> <b>@Kyristepiu</b></a></li>
@@ -53,6 +64,10 @@ include 'header.html';
 						<li><a href="https://twitter.com/SilveripGT" onclick="window.open(this.href); return false;"><i class="fab fa-twitter"></i> <b>@SilveripGT</b></a></li>
 						<li><a href="https://thesilverip.wordpress.com" onclick="window.open(this.href); return false;"><i class="fab fa-wordpress"></i> <b>@theSilverip</b></a></li>
 					</ul>
+                                Stories en cours/réalisées : 
+                                    <ul>
+                                        <li><a href="https://fmslife.fr/t/road-trip-in-america/3540" onclick="window.open(this.href); return false;">FM18 - Road trip in America ft. Kyristepiu</a></li>
+                                    </ul>
 				</p>
 				
 				
